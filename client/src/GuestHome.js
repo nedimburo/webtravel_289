6e1e0d2e04b63ebfdcf169ba4673ea3f4732cdd1
nedimburo <nedimb9402@gmail.com>
2024-02-12 15:41:07 +0100

    Added filtration system for travels based on the selected category

--- client/src/GuestHome.js
+++ client/src/GuestHome.js
@@ -4,12 +4,17 @@ import axios from "axios";
 
 function GuestHome(){
     const [travels, setTravels] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
     useEffect(()=>{
         axios.get('http://localhost:3001/travel/get-travels')
         .then(response=>setTravels(response.data))
         .catch(error=>console.error("Error fetching travel informations", error));
     }, []);
+
+    const filteredTravels = selectedCategory
+    ? travels.filter((travel) => travel.category === selectedCategory)
+    : travels;
 
     if (!travels){
         return <div>Loading Travels...</div>
@@ -39,10 +44,23 @@ function GuestHome(){
                         Suspendisse vulputate tortor ut ultrices venenatis."</p>
                 </div>
                 <h2 className='text-center'>Travel Offers:</h2>
-                {travels.length === 0 ? (
+                <div className='text-center filter-buttons-container'>
+                    <p className='small-container-text'>Filter by:</p>
+                    <button onClick={() => setSelectedCategory('Europe')} className='btn btn-success'>Europe</button>
+                    <button onClick={() => setSelectedCategory('America')} className='btn btn-success'>America</button>
+                    <button onClick={() => setSelectedCategory('Australia')} className='btn btn-success'>Australia</button>
+                    <button onClick={() => setSelectedCategory('Asia')} className='btn btn-success'>Asia</button>
+                    <button onClick={() => setSelectedCategory('Africa')} className='btn btn-success'>Africa</button>
+                    <button onClick={() => setSelectedCategory('Short Trips')} className='btn btn-success'>Short Trips</button>
+                    <button onClick={() => setSelectedCategory('Long Trips')} className='btn btn-success'>Long Trips</button>
+                    <button onClick={() => setSelectedCategory('Summer')} className='btn btn-success'>Summer</button>
+                    <button onClick={() => setSelectedCategory('Winter')} className='btn btn-success'>Winter</button>
+                    <button onClick={() => setSelectedCategory('')} className='btn btn-warning'>RESET</button>
+                </div>
+                {filteredTravels.length === 0 ? (
                     <p className='text-center warning-message'>No travels currently available.</p>
                 ) : (
-                    travels.map((travel, index) => (
+                    filteredTravels.map((travel, index) => (
                         <div key={index} className='travel-home-container'>
                             <div className='travel-home-image-container'>
                                 <img src='./images/travelicon.png' className='scale-image' alt='Travel Icon'/>
--- client/src/Home.js
+++ client/src/Home.js
@@ -6,6 +6,7 @@ import axios from "axios";
 function Home(){
     const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem("user")));
     const [travels, setTravels] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
     useEffect(() => {
         setUserInfo(JSON.parse(localStorage.getItem("user")));
@@ -16,6 +17,10 @@ function Home(){
         .then(response=>setTravels(response.data))
         .catch(error=>console.error("Error fetching travel informations", error));
     }, []);
+
+    const filteredTravels = selectedCategory
+    ? travels.filter((travel) => travel.category === selectedCategory)
+    : travels;
 
     if (!userInfo){
         return <div>Loading Loggedin User Information...</div>
@@ -50,10 +55,23 @@ function Home(){
                         Suspendisse vulputate tortor ut ultrices venenatis."</p>
                 </div>
                 <h2 className='text-center'>Travel Offers:</h2>
-                {travels.length === 0 ? (
+                <div className='text-center filter-buttons-container'>
+                    <p className='small-container-text'>Filter by:</p>
+                    <button onClick={() => setSelectedCategory('Europe')} className='btn btn-success'>Europe</button>
+                    <button onClick={() => setSelectedCategory('America')} className='btn btn-success'>America</button>
+                    <button onClick={() => setSelectedCategory('Australia')} className='btn btn-success'>Australia</button>
+                    <button onClick={() => setSelectedCategory('Asia')} className='btn btn-success'>Asia</button>
+                    <button onClick={() => setSelectedCategory('Africa')} className='btn btn-success'>Africa</button>
+                    <button onClick={() => setSelectedCategory('Short Trips')} className='btn btn-success'>Short Trips</button>
+                    <button onClick={() => setSelectedCategory('Long Trips')} className='btn btn-success'>Long Trips</button>
+                    <button onClick={() => setSelectedCategory('Summer')} className='btn btn-success'>Summer</button>
+                    <button onClick={() => setSelectedCategory('Winter')} className='btn btn-success'>Winter</button>
+                    <button onClick={() => setSelectedCategory('')} className='btn btn-warning'>RESET</button>
+                </div>
+                {filteredTravels.length === 0 ? (
                     <p className='text-center warning-message'>No travels currently available.</p>
                 ) : (
-                    travels.map((travel, index) => (
+                    filteredTravels.map((travel, index) => (
                         <div key={index} className='travel-home-container'>
                             <div className='travel-home-image-container'>
                                 <img src='./images/travelicon.png' className='scale-image' alt='Travel Icon'/>
